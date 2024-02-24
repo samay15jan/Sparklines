b5c3e89f9e67f51efef8d34fd81abca9b41256ad
samay15jan <samay15jan@gmail.com>
2024-02-24 23:32:23 +0530

    added google auth

---
+++ src/components/utils/auth.js
@@ -0,0 +1,33 @@
+import { auth, database } from './firebase.js'
+import { GoogleAuthProvider, signOut, signInWithPopup } from "firebase/auth";
+import { set, ref } from 'firebase/database';
+
+const googleLogin = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user
+        const userId = user.uid
+        set(ref(database, 'sparklines/users/' + userId), {
+          name: user.displayName,
+          email: user.email,
+          profile : user.photoURL,
+        })
+      })
+      .catch((error) => {
+        console.error('Google login error:', error)
+      })
+}
+
+const logout = async () => {
+  try {
+    await signOut(auth)
+    console.log('signed out')
+    return
+  } 
+  catch (error) {
+    throw error
+  }
+}
+
+export {googleLogin, logout}
