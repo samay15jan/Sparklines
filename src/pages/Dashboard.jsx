3d8445d0df4c282ddbbc2fb4e54bfb053e85df6b
samay15jan <samay15jan@gmail.com>
2024-03-17 06:09:08 +0530

    added fallback

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import AutoNavigate from '../utils/AutoNavigate'
+const FirstLogin = lazy(() => import('../components/initialLogin/FirstLogin'))
 
 const Dashboard = () => {
+  const newUser = localStorage.getItem('newUser')
+
   return (
-    <div>
+    <div className='min-h-screen items-center grid-cols-1 grid w-screen overflow-hidden bg-black text-white'>
       <AutoNavigate />
-      Dashboard
+      {newUser && <FirstLogin />}
     </div>
   )
 }
--- src/utils/Loading.jsx
+++ src/utils/Loading.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
 import { LineWave } from 'react-loader-spinner'
 
+const Container = styled.div`${tw`w-full h-screen flex flex-col items-center justify-center`}`
+const Text = styled.div`${tw`text-xl text-[#23233f]`}`
+
 const Loading = () => {
+    const [selected, setSelected] = useState('')
+    const quotes = {
+        1: "Jamming in progress... Just hold the applause",
+        2: "Please stand by... Finding the perfect beat takes time!",
+        3: "Almost there! Just tuning the virtual instruments",
+        4: "Hold tight! We're spinning the digital vinyl",
+        5: "Just a sec! Quieting the noisy internet neighbors",
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            const number = Math.floor(Math.random() * 6)
+            setSelected(quotes[number])    
+        }, 50);
+    }, [])
+
     return (
-        <div className='w-screen min-h-screen items-center flex justify-center'>
+        <Container>
             <LineWave
                 color="#23233f"
                 visible={true}
@@ -11,7 +32,8 @@ const Loading = () => {
                 width="150"
                 ariaLabel="line-wave-loading"
             />
-        </div>
+            <Text>{selected}</Text>
+        </Container>
     )
 }
 
