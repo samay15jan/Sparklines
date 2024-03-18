e09fa4d2c74c1821205faef9b194249d7f65a463
samay15jan <samay15jan@gmail.com>
2024-03-18 23:10:20 +0530

    build languages saving api

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -4,11 +4,8 @@ const jwt = require('jsonwebtoken')
 // Register new user
 const register = async (req, res, next) => {
     const { email, password } = req.body
-    const username = 'user' + Math.round(Math.random(1042313) * 2314231)
-    const newUser = true 
-    const profilePic = 'https://res.cloudinary.com/sparklines/image/upload/v1710355835/default/bzcj4ipftbmo48v30din.png' 
     try {
-        const user = User({ username, email, password, profilePic, newUser })
+        const user = User({ email, password })
         await user.save()
         res.status(201).json({ message: 'User Registered' })
     } catch (error) {
--- backend/src/middlewares/userData.js
+++ backend/src/middlewares/userData.js
@@ -5,7 +5,7 @@ const User = require('../models/UserSchema')
 const updateData = async (req, res, next) => {
   const { username, profilePic, userId } = req.body
 
-  if(!username){
+  if (!username) {
     return res.status(401).json({ message: 'An Error Occured' })
   }
 
@@ -15,25 +15,21 @@ const updateData = async (req, res, next) => {
       { username: username, profilePic: profilePic },
       { new: true }
     )
-
     if (!user) {
       return res.status(404).json({ message: 'User does not exist' })
-      
     }
     req.user = user
     next()
   } catch (error) {
-      return res.status(500).json({ message: 'An Error Occured' })
+    return res.status(500).json({ message: 'An Error Occured' })
   }
 }
 
 
 const imageUploader = async (req, res, next) => {
   const profilePic = req.file
-
   if (!profilePic) {
     return res.status(404).json({ message: 'Image not found!' })
-    
   }
 
   const config = {
@@ -46,16 +42,38 @@ const imageUploader = async (req, res, next) => {
     cloudinary.config(config)
     const result = await cloudinary.uploader.upload(profilePic.path, { public_id: profilePic.originalname })
     fs.unlinkSync(profilePic.path)
-
-    if(!result){
-      return res.status(401).json({ message: 'An error occured!' })    
+    if (!result) {
+      return res.status(401).json({ message: 'An error occured!' })
     }
-    
     req.profilePic = result.secure_url
-    next()    
+    next()
   } catch (error) {
-      return res.status(500).json({ message: 'Failed to upload' })
+    return res.status(500).json({ message: 'Failed to upload' })
   }
 }
 
-module.exports = { updateData, imageUploader }
+
+const addLanguages = async (req, res, next) => {
+  const { languages, userId } = req.body
+
+  if (!languages || !userId) {
+    return res.status(401).json({ message: 'An error occured' })
+  }
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: userId },
+      { languages: languages },
+      { new: true }
+    )
+    if (!user) {
+      return res.status(404).json({ message: 'User does not exist' })
+    }
+    req.languages = user.languages
+    next()
+  } catch (error) {
+    return res.status(500).json({ message: 'Failed to upload' })
+  }
+}
+
+module.exports = { updateData, imageUploader, addLanguages }
