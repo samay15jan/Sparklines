a0ed454e17a6d4f7932b087ba05d45558cef3246
samay15jan <samay15jan@gmail.com>
2024-03-30 14:35:16 +0530

    fixed search

--- src/components/dashboard/SearchMenu/Input.jsx
+++ src/components/dashboard/SearchMenu/Input.jsx
@@ -3,7 +3,13 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 const Container = styled.div`${tw`w-screen flex justify-center`}`
-const SearchInput = styled.input`${tw`text-lg px-10 my-2 py-2 border border-2 rounded-full drop-shadow-md transition-all duration-300 ease-in-out`}
+const SearchInput = styled.input`${tw`bg-[#242424] text-white text-lg px-10 my-2 py-2 rounded-full drop-shadow-md transition-all duration-300 ease-in-out`}
+&:hover {
+    background: #2a2a2a;
+}
+&:focus {
+  outline: 2px solid white;
+}
 ${({ selected }) => selected ? tw`w-screen mx-20 text-left` : tw`w-1/3 text-center`};
 `
 
@@ -19,6 +25,7 @@ const Input = ({ close, SearchText }) => {
     return (
         <Container>
             <SearchInput
+                accessKey='s'
                 onClick={() => setSelected(!selected)}
                 type="text"
                 selected={selected}
