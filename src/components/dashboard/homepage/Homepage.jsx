f40b23d7cd9fc13cc9f22b7dbecfbdcc16d3b3f3
samay15jan <samay15jan@gmail.com>
2024-04-29 03:44:48 +0530

    Finished Homepage

--- src/components/dashboard/homepage/Homepage.jsx
+++ src/components/dashboard/homepage/Homepage.jsx
@@ -1,21 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import { homepageData } from '../../../utils/apiMethods'
+import Carousel from './Carousel'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const Container = styled.div`${tw`m-10`}`
+const Heading = styled.div`${tw`mt-5 text-2xl font-bold`}`
 
 const Homepage = () => {
   const [data, setData] = useState()
 
-  useEffect( () => {
-    
+  useEffect(() => {
+    getData()
   }, [])
 
-  async function getData(){
+  async function getData() {
     const response = await homepageData()
     setData(response)
   }
 
   return (
-    <div>
-    </div>
+    <Container>
+      <Heading>Trending Songs</Heading>
+      {data &&
+        <Carousel CarouselData={data.data.trending.songs} />
+      }
+
+      <Heading>Playlists you can't miss</Heading>
+      {data &&
+        <Carousel CarouselData={data.data.playlists} />
+      }
+
+      <Heading>Popular Albums</Heading>
+      {data &&
+        <Carousel CarouselData={data.data.albums} />
+      }
+
+      <Heading>Top Charts</Heading>
+      {data &&
+        <Carousel CarouselData={data.data.charts} />
+      }
+    </Container>
   )
 }
 
--- src/utils/apiMethods.js
+++ src/utils/apiMethods.js
@@ -3,13 +3,18 @@ import axios from "axios"
 async function homepageData() {
   try {
     const userId = localStorage.getItem('userId')
-    const headers = {
-      'userid': `${userId}`,
-      'Content-Type': 'application/json'
-    }
     const language = localStorage.getItem('languages')
-    const params = { language: language.toLowerCase() }
-    const response = await axios.get('/api/modules', params, { headers })
+    const options = {
+      method: 'GET',
+      url: '/api/modules',
+      params: { language: language.toLowerCase() },
+      headers: {
+        'userid': `${userId}`,
+        'Content-Type': 'application/json'
+      }
+  }
+
+    const response = await axios.request(options)
     if (response && response.status === 200) {
       return response.data
     }
