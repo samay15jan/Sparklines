b73691cd6d5737a75e69420dd10e6bf708ee8194
samay15jan <samay15jan@gmail.com>
2024-05-05 03:36:24 +0530

    fixed bugs

--- src/components/dashboard/playback/AudioDetails.jsx
+++ src/components/dashboard/playback/AudioDetails.jsx
@@ -8,12 +8,22 @@ const SubHeading = styled.div`${tw`px-1 text-[12px] opacity-50`}`
 
 const AudioDetails = ({ details }) => {
   return (
-    <>
-      <Image src={details.image[0].link} alt='' />
-      <div className='grid grid-rows-2'>
-        <Heading>{details.name}</Heading>
-        <SubHeading>{details.primaryArtists}</SubHeading>
-      </div>
+    <>{details
+      ? <>
+        <Image src={details?.image[0]?.link} alt='' />
+        <div className='grid grid-rows-2'>
+          <Heading>{details?.name}</Heading>
+          <SubHeading>{details?.primaryArtists}</SubHeading>
+        </div>
+      </>
+      : <>
+        <div className='w-12 h-12 m-2 rounded-md animate-pulse bg-white opacity-20' />
+        <div className='grid grid-rows-2 opacity-20'>
+          <Heading className='w-40 h-4 rounded-md animate-pulse bg-white' />
+          <SubHeading className='w-28 h-4 rounded-md animate-pulse bg-white' />
+        </div>
+      </>
+    }
     </>
   )
 }
--- src/components/dashboard/playback/Playback.jsx
+++ src/components/dashboard/playback/Playback.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState, lazy } from 'react'
 import { playbackSong } from '../../../utils/apiMethods'
 import styled from 'styled-components'
 import tw from 'twin.macro'
+
 const AudioDetails = lazy(() => import('./AudioDetails'))
 const AudioVisualizer = lazy(() => import('./AudioVisualizer'))
 const AudioController = lazy(() => import('./AudioController'))
 const MenuButtons = lazy(() => import('./MenuButtons'))
 const VolumeController = lazy(() => import('./VolumeController'))
-
 
 const Container = styled.div`${tw`fixed bottom-0 bg-black w-screen p-2 text-sm font-semibold`}`
 const SubContainer = styled.div`${tw`grid grid-cols-3 justify-between`}`
@@ -31,15 +31,13 @@ const Playback = () => {
 
   return (
     <Container>
-      {
-        data &&
         <SubContainer>
           <div className='flex'>
-            <AudioDetails details={data.data[0]} />
+            <AudioDetails details={data?.data[0]} />
             <AudioVisualizer show={isPlaying} />
           </div>
           <AudioController
-            audioSrc={data.data[0].downloadUrl[4].link}
+            audioSrc={data?.data[0]?.downloadUrl[4].link}
             returnPlaying={(value) => setPlaying(value)}
             returnPlayerRef={(e) => setAudioPlayer(e)}
           />
@@ -48,7 +46,6 @@ const Playback = () => {
             <VolumeController audioPlayer={audioPlayer} />
           </div>
         </SubContainer>
-      }
     </Container>
   )
 }
