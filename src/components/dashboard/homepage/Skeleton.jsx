393885b41eded467d951f81a06cc7d73bd366367
samay15jan <samay15jan@gmail.com>
2024-05-05 03:20:28 +0530

    added skeleton fallback

---
+++ src/components/dashboard/homepage/Skeleton.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+
+const Container = styled.div`${tw`p-2 cursor-pointer grid grid-flow-col overflow-x-auto text-sm font-bold`}`
+const SubContainer = styled.div`${tw`grid grid-cols-1 p-5 w-48 rounded-xl opacity-20`}
+transition: background 0.3s ease;
+&:hover {
+  background: #2a2a2a;
+}`
+const ImageSkeleton = styled.div`${tw`w-36 h-36 rounded-xl bg-white animate-pulse`}`
+const TitleSkeleton = styled.div`${tw`w-32 h-4 mx-1 mt-2 rounded-md bg-white animate-pulse`}`
+const SubTitileSkeleton = styled.div`${tw`w-28 h-4 mx-1 mt-2 rounded-md bg-white animate-pulse`}`
+
+const Skeleton = () => {
+  const fallback = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  return (
+    <Container>
+      {fallback.map((type) => (
+        <SubContainer>
+          <ImageSkeleton key={type} />
+          <TitleSkeleton />
+          <SubTitileSkeleton />
+        </SubContainer>
+      ))}
+    </Container>
+
+  )
+}
+
+export default Skeleton
