542bf5b3edd2ff437703d42f39ccfeefb10b027b
samay15jan <samay15jan@gmail.com>
2024-05-21 08:35:42 +0530

    added audio player demo

--- src/components/landing/header/Header.jsx
+++ src/components/landing/header/Header.jsx
@@ -1,8 +1,11 @@
 import React, { lazy } from 'react'
+import { useNavigate } from 'react-router-dom'
 const LogoContainer = lazy(() => import('./LogoContainer'))
 const MainLogo = lazy(() => import('./MainLogo'))
 
 const Header = () => {
+  const navigate = useNavigate()
+
   return (
     <div className='p-5 grid grid-cols-4 w-auto'>
       <div className='flex gap-4'>
@@ -12,7 +15,7 @@ const Header = () => {
       <MainLogo text='Sparklines' />
       <div className='flex justify-center gap-4'>
         <LogoContainer text='Github' />
-        <LogoContainer text='Login' whiteBg='true' />
+        <LogoContainer text='Login' whiteBg='true' navigation={(location) => navigate(location)} />
       </div>
     </div>
   )
