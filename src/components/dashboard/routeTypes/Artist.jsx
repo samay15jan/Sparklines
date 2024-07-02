5acc89b466a7ffee8fb07bb8e9f8ba31cd3a94db
Samay Kumar <samay15jan@gmail.com>
2024-07-02 22:37:02 +0530

    building other page routes

--- src/components/dashboard/routeTypes/Artist.jsx
+++ src/components/dashboard/routeTypes/Artist.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  artistDetails,
+  artistSongs,
+  artistAlbums,
+  artistRecommendations
+} from '../../../utils/apiMethods'
 
-const Artist = () => {
+const artist = () => {
+  const [data, setData] = useState()
+  const { id } = useParams()
+
+  useEffect(() => {
+    getData()
+  }, [data])
+
+  async function getData() {
+    if (id) {
+      const response = await artistDetails(id)
+      setData(response)
+    }
+  }
+
   return (
-    <div>Artist</div>
+    <div>
+
+    </div>
   )
 }
 
-export default Artist
+export default artist
--- src/components/dashboard/routeTypes/Playlist.jsx
+++ src/components/dashboard/routeTypes/Playlist.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { playlistDetails } from '../../../utils/apiMethods'
 
 const Playlist = () => {
+  const [data, setData] = useState()
+  const { id } = useParams()
+
+  useEffect(() => {
+    getData()
+  }, [data])
+
+  async function getData() {
+    if (id) {
+      const response = await playlistDetails(id)
+      setData(response)
+    }
+  }
+
   return (
-    <div>Playlist</div>
+    <div>
+
+    </div>
   )
 }
 
